214fd24c9a03ed6b5fe98a0e818302b801d8c693
Chstphr <somto565@gmail.com>
2023-04-24 11:50:12 +0200

    input function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import './App.css';
-import { Auth } from './components/Auth';
+import React, { useState } from "react";
+import "./App.css";
+import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
 
+const cookies = new Cookies();
 
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState("");
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth />
+      </div>
+    );
+  }
+
   return (
-   <div>
-   <Auth />
-   </div>
+    <div>
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label></label>{" "}
+        </div>
+      )}
+    </div>
   );
 }
 
