d63e8d109f132c27147655b3f8472e2622607cc9
Chstphr <somto565@gmail.com>
2023-04-24 07:53:59 +0200

    created a cookie function

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,16 +1,22 @@
-import { signInWithPopup } from 'firebase/auth'
-import { auth, provider } from '../firebase-config'
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebase-config";
+import Cookies from "universal-cookie";
 
+const cookies = new Cookies();
 
 export const Auth = () => {
-    const signInWithGoogle = async () => {
-         const result = await signInWithPopup(auth, provider)
-         console.log(result)
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+    } catch (err) {
+      console.error(err);
     }
-    return (
-        < div className='auth'>
-            <p> Sign In With Google To Continue</p>
-            <button onClick={signInWithGoogle}> Sign In With Google</button>
-        </div>
-    )
-}
+  };
+  return (
+    <div className="auth">
+      <p> Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}> Sign In With Google</button>
+    </div>
+  );
+};
