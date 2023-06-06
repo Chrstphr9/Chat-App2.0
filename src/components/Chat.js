c0be71ca7ab769324c062313086f81a9874d000c
Chstphr <somto565@gmail.com>
2023-06-06 12:45:36 +0200

    added style component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,3 +1,40 @@
-export const Chat = () => {
-    return <div className="chat-app"> Chat </div>
-}
+import {useState} from "react"
+import { addDoc,collection, serverTimestamp } from 'firebase/firestore'
+import { auth, db } from '../firebase-config';
+
+export const Chat = (props) => {
+    const {room} = props
+    const [newMessage, setNewMessage] = useState("");
+
+    const messageRef = collection(db, 'messages')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (newMessage === "") return;
+
+        await addDoc(messageRef, {
+            text: newMessage,
+            createdAt: serverTimestamp(),
+            user: auth.currentUser.displayName,
+            room,
+        });
+
+        setNewMessage('')
+    };
+
+  return (
+    <div className="chat-app">
+      <form onSubmit={handleSubmit} className="new-message-form">
+        <input
+          className="new-message-input"
+          placeholder="Type Your Message Here....."
+          onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
+        />
+        <button type="submit" className="send-button">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
