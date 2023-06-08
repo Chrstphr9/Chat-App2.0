6875d170c081ab8f1c4863c9b480b0a75dbf3e41
Chstphr <somto565@gmail.com>
2023-06-08 09:10:30 +0200

    its just working

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,29 +1,62 @@
-import {useState} from "react"
-import { addDoc,collection, serverTimestamp } from 'firebase/firestore'
-import { auth, db } from '../firebase-config';
+import { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+import "./styles/Chat.css";
 
 export const Chat = (props) => {
-    const {room} = props
-    const [newMessage, setNewMessage] = useState("");
+  const { room } = props;
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    const messageRef = collection(db, 'messages')
+  const messageRef = collection(db, "messages");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (newMessage === "") return;
-
-        await addDoc(messageRef, {
-            text: newMessage,
-            createdAt: serverTimestamp(),
-            user: auth.currentUser.displayName,
-            room,
-        });
-
-        setNewMessage('')
+  useEffect(() => {
+    const queryMessages = query(messageRef, where("room", "==", room));
+    // onSnapshot(queryMessages, (snapShot) => {
+    //   console.log("New Message")
+    // });
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => {
+      unsubscribe();
     };
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newMessage === "") return;
+
+    const userDisplayName = auth.currentUser?.displayName;
+
+    await addDoc(messageRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room,
+    });
+
+    setNewMessage("");
+  };
 
   return (
     <div className="chat-app">
+      <div>
+        {messages.map((message) => (
+          <h1> {message.text} </h1>
+        ))}
+      </div>
       <form onSubmit={handleSubmit} className="new-message-form">
         <input
           className="new-message-input"
